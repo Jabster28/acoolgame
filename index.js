97137cdd3cf7b49ac015c2c0fd368f52353195f8
Jabster28 <justynboyer@gmail.com>
2019-02-28 16:46:11 +0000

    switch to browserify

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express()
 var expressBrowserify = require('express-browserify');
 app.use("/", express.static(__dirname + "/public/"))
 app.use("/lib/orchestre.min.js", express.static(__dirname + "/lib/orchestre.min.js"))
-app.get("/script.js", express.static(__dirname + "/public/script.js"));
+// app.get("/script.js", express.static(__dirname + "/public/script.js"));
+app.get("/script.js", expressBrowserify("public/script.js"));
 app.use("/1.ogg", express.static(__dirname + "/res/1.ogg"))
 const port = 3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
