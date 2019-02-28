df458c877cb8887891ead72fe2c61ebb8353b0b1
Jabster28 <justynboyer@gmail.com>
2019-02-28 17:47:02 +0000

    fixed it again

--- public/script.js
+++ public/script.js
@@ -4,8 +4,8 @@ rythm.setMusic('res/cyc.mp3')
 rythm.addRythm('bassjump', 'jump', 500, 100)
 rythm.addRythm('trebleshake', 'twist', 0, 10, {
   radius: 5,
-  min: 5,
-  max: 5
+  min: -10,
+  max: 10
 })
 
 rythm.start()
