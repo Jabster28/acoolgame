07f51e2ebf9e37b5b1d20206b1b7e2b4b9f1ce3e
Jabster28 <justynboyer@gmail.com>
2019-02-28 17:40:49 +0000

    TEST

--- public/script.js
+++ public/script.js
@@ -1,23 +1,20 @@
-$(function() {
-  // rythm
-  var rythm = new Rythm()
+// rythm
+var rythm = new Rythm()
 rythm.setMusic('res/cyc.mp3')
 rythm.addRythm('bassjump', 'jump', 500, 100)
 rythm.addRythm('trebleshake', 'twist', 0, 10, {
-  radius: 10
+  radius: -100
 })
 
 rythm.start()
-  // orchestra
-  console.log("hello")
-  const orchestra = new Orchestre(155);
-  orchestra.addPlayer('1', './1.ogg', 98)
-    .then(() => { /* Called once the player is loaded */
-      orchestra.start()
-      orchestra.play('1');
-      orchestra.toggle("1")
-      $("button").click(function() {
-        orchestra.toggle("1")
-      })
-    });
-})
+// orchestra
+console.log("hello")
+const orchestra = new Orchestre(155);
+orchestra.addPlayer('1', './1.ogg', 98).then(() => { /* Called once the player is loaded */
+  orchestra.start()
+  orchestra.play('1');
+  orchestra.toggle("1")
+  $("button").click(function() {
+    orchestra.toggle("1")
+  })
+});
