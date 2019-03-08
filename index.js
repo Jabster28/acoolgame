c6b146309f228ce60573198641c915b674561cbe
Justyn Boyer <29015942+Jabster28@users.noreply.github.com>
2019-03-08 12:51:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -35,28 +35,6 @@ app.use(bodyParser());
 var expressBrowserify = require('express-browserify');
 app.use("/", express.static(__dirname + "/public/"))
 app.use("/a/", express.static(__dirname + "/public/a.html"))
-app.get("/api", function(q, s){
-s.set('Content-Type', 'application/json');
-	s.send(stringify(q))
-})
-app.post("/api/newplayer", function(q, s){
-	name = q.body.name
-	stuff = []
-s.set('Content-Type', 'application/json');
-	for (i = 0; i < users.length; i++){
-		stuff.push(users[i].name)
-	}
-	if (!(stuff.includes(name))){
-users.push({
-	"name": q.body.name,
-	"x": "0",
-	"y": "0",
-})
-s.send({"num": users.length - 1, "u": users})
-} else {
-s.send({"err": "Name Already Taken"})
-}
-})
 app.use("/lib/orchestre.min.js", express.static(__dirname + "/lib/orchestre.min.js"))
 // app.get("/script.js", express.static(__dirname + "/public/script.js"));
 app.get("/script.js", expressBrowserify("public/script.js"));
